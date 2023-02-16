ebed5f07e0301535b16cf9143c5e14312d4ea5b3
vandajl1 <steamervandajl1@gmail.com>
2023-02-16 08:45:41 +0400

    Add files via upload

---
+++ src/main/java/entity/User.java
@@ -0,0 +1,12 @@
+package entity;
+
+import lombok.Data;
+import lombok.AllArgsConstructor;
+
+@Data
+@AllArgsConstructor
+public class User {
+    private String email;
+    private String password;
+    private String name;
+}
